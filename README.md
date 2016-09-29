878920e9656abd2394643b0cbbaa4cc391c51d95
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2016-09-29 17:45:31 +0200

    Travis: Bug fixes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Orthrus [![Build Status](https://travis-ci.org/test-pipeline/orthrus.svg?branch=master)](https://travis-ci.org/test-pipeline/orthrus)
+# Orthrus [![Build Status](https://travis-ci.org/test-pipeline/orthrus.svg?branch=master)](https://travis-ci.org/test-pipeline/orthrus) [![License](http://img.shields.io/:license-gpl3-blue.svg?style=flat-square)](http://www.gnu.org/licenses/gpl-3.0.html)
 
 A tool for managing, conducting, and assessing security (fuzz) testing.
--- install_deps.sh
+++ install_deps.sh
@@ -14,8 +14,9 @@ libtoolize --force
 aclocal && automake --force-missing --add-missing && autoconf
 cd ../../
 echo -e "\t[+] Installing afl-utils"
-git clone https://github.com/rc0r/afl-utils.git
-cd afl-utils && sudo mkdir -p /usr/lib/python3.4/site-packages && sudo python3 setup.py install
+wget -q https://github.com/rc0r/afl-utils/archive/v1.27a.tar.gz && tar xzf v1.27a.tar.gz
+rm v1.27a.tar.gz && cd afl-utils-1.27a
+sudo mkdir -p /usr/lib/python3.4/site-packages && sudo python3 setup.py install
 cd ../
 echo -e "\t[+] Setting up exploitable"
 echo "source /usr/lib/python3.4/site-packages/exploitable-1.32_rcor-py3.4.egg/exploitable/exploitable.py" >> ~/.gdbinit
