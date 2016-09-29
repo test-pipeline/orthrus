eae5e7f157865793c54e8369757b1463c632e61a
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2016-09-29 15:03:45 +0200

    Travis: Bug fixes

---
+++ install_deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+wget -q http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz &> /dev/null
+tar xzf afl-latest.tgz &> /dev/null
+rm -f afl-latest.tgz && cd afl-*
+sudo make install
