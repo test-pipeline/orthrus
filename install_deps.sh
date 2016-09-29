86916e78029d75bfeeb7f53269bd74e5a67919cc
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2016-09-29 16:10:01 +0200

    Travis: Bug fixes

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+echo -e "\t[+] Fetching afl-latest"
 wget -q http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz &> /dev/null
 tar xzf afl-latest.tgz &> /dev/null
 rm -f afl-latest.tgz && cd afl-*
+echo -e "\t[+] Installing afl"
 sudo make install
+cd ..
+echo -e "\t[+] Setting core_pattern"
 echo core | sudo tee /proc/sys/kernel/core_pattern
+echo -e "\t[+] Running autotools in test dir"
+cd testdata/Automake-Autoconf-Template-Project
+libtoolize --force
+aclocal && autoheader && automake --force-missing --add-missing && autoconf
+cd ../../
