d67b639c6ebb24d8eebd9076fd6c48d918e1196f
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2017-02-20 15:50:21 +0100

    Update Travis CI gdbinit

--- install_deps.sh
+++ install_deps.sh
@@ -22,6 +22,10 @@ echo -e "\t[+] Setting up GDB and exploitable"
 cat <<EOF >> ~/.gdbinit
 source /usr/lib/python3.4/site-packages/exploitable-1.32_rcor-py3.4.egg/exploitable/exploitable.py
 source ~/.orthrus/gdb_orthrus.py
+define hook-quit
+    set confirm off
+end
+set pagination off
 EOF
 echo -e "\t[+] Installing afl-cov"
 wget -q https://github.com/mrash/afl-cov/archive/0.6.tar.gz && tar xzf 0.6.tar.gz
