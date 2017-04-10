3d84ac4bec1d109e01a67cf42e44593229d43a9a
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2017-04-10 15:09:23 +0200

    Travis: Debug

--- builder/builder.py
+++ builder/builder.py
@@ -117,5 +117,7 @@ class Builder(object):
         command = ["find . -type f \( -name \"*.c\" -o -name \"*.cpp\" -o -name \"*.cc\" \) -print0 |"
                    " xargs -0 clang-sdict -p 1>> dict.clang"]
         if not util.run_cmd(command, self.env, self.logfile):
+            with open(self.logfile, 'r') as fin:
+                print fin.read()
             return False
         return True
